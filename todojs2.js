fec1a2aae18cbda37129b51b0f0ec26ef39134e2
shamssar <nayefshams3@gmail.com>
2022-04-19 18:35:27 +0300

    add local line

---
+++ todojs2.js
@@ -0,0 +1,32 @@
+let quess1 = prompt("It is a lovely Day? (Y/N)"); 
+let quess2 = prompt("Are you Happy? (Y/N)");
+let quess3 = prompt(" is everything okay? (Y/N)");
+
+
+
+if(quess1==false)
+{
+    quess1="invaled";
+}
+else if (quess2==false){
+
+    quess2="invaled";
+
+}
+else {(quess3==false)
+
+    quess3="invaled";
+}
+let UserAnswers=[quess1,quess2,quess3]
+
+for (var i=0 ;i<UserAnswers.lenght; i++){
+
+    console.log(UserAnswers);
+    }
+
+
+    console.log(UserAnswers);
+
+      
+      
+           
