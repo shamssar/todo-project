261c485f4a4af2ebb9f9481c4f0a50a176cc7b99
shamssar <nayefshams3@gmail.com>
2022-04-20 12:14:16 +0300

    add local

--- todojs2.js
+++ todojs2.js
@@ -1,32 +1,30 @@
-let quess1 = prompt("It is a lovely Day? (Y/N)"); 
-let quess2 = prompt("Are you Happy? (Y/N)");
-let quess3 = prompt(" is everything okay? (Y/N)");
+
+let quess1 = AskQuess("It is a lovely Day? (Y/N)"); 
+let quess2 = AskQuess("Are you Happy? (Y/N)");
+let quess3 = AskQuess(" is everything okay? (Y/N)");
 
 
 
-if(quess1==false)
+
+let UserAnswers=[quess1,quess2,quess3];
+MyAnswer(UserAnswers);
+
+
+function AskQuess(question)
 {
-    quess1="invaled";
+    let UserAnswers= prompt(question);
+    return UserAnswers
 }
-else if (quess2==false){
-
-    quess2="invaled";
-
-}
-else {(quess3==false)
-
-    quess3="invaled";
-}
-let UserAnswers=[quess1,quess2,quess3]
-
-for (var i=0 ;i<UserAnswers.lenght; i++){
-
-    console.log(UserAnswers);
+function MyAnswer(arr) {
+    for (let i= 0; i< arr.length; i++ ) {
+        if (arr[i] === "") {
+            console.log("invalid");
+        }
+        else {
+            console.log(arr[i]);
+        }
     }
-
-
-    console.log(UserAnswers);
-
+}
       
       
            
